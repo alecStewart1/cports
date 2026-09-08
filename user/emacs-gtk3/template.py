89914608642ddbf33cafe1742b1efd12254f9b19
Alec Stewart <alec-stewart@pm.me>
2026-09-08 08:35:56 -0500

    [local] user/emacs-*: upgrade to 31.1

--- user/emacs-gtk3/template.py
+++ user/emacs-gtk3/template.py
@@ -1,6 +1,6 @@
 pkgname = "emacs-gtk3"
-pkgver = "30.2"
-pkgrel = 2
+pkgver = "31.1"
+pkgrel = 0
 build_style = "gnu_configure"
 configure_args = [
     "--with-gameuser=:_games",
@@ -18,11 +18,13 @@ make_check_args = [
     "EXCLUDE_TESTS="
     " %eglot-tests.el"  # requires a variety of lsp servers
     " %tramp-tests.el"  # TODO: fails mysteriously
+    " %package-vc-tests.el"  # TODO: hangs
     " %shr-tests.el"  # TODO: zoom-image times out
     " %process-tests.el"  # TODO: times out
 ]
 hostmakedepends = [
     "automake",
+    "ctags",
     "gawk",
     "pkgconf",
     "texinfo",
@@ -52,7 +54,8 @@ makedepends = [
     "sqlite-devel",
     "tree-sitter-devel",
 ]
-depends = ["libgccjit"]
+checkdepends = ["bash", "git", "mandoc"]
+depends = ["libgccjit", "ctags"]
 provides = [f"emacs={pkgver}"]
 provider_priority = 30
 replaces = [f"emacs-gtk3~{pkgver}"]
@@ -60,7 +63,7 @@ pkgdesc = "Extensible, customizable, self-documenting, real-time display editor"
 license = "GPL-3.0-or-later"
 url = "https://www.gnu.org/software/emacs/emacs.html"
 source = f"$(GNU_SITE)/emacs/emacs-{pkgver}.tar.xz"
-sha256 = "b3f36f18a6dd2715713370166257de2fae01f9d38cfe878ced9b1e6ded5befd9"
+sha256 = "1da5790d9580c81932b5bf700633114468da7b3412d69faa767daebf974f4586"
 
 
 def post_install(self):
@@ -74,7 +77,3 @@ def post_install(self):
 
     self.uninstall("usr/lib/systemd/user")
     self.uninstall("var/games")
-
-    # conflicts with ctags
-    self.rename("usr/bin/ctags", "ctags.emacs")
-    self.rename("usr/share/man/man1/ctags.1.gz", "ctags.emacs.1.gz")
