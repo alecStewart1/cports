0367a87ad9cc86bd9e3008232a77a1dcc67f8242
Alec Stewart <alec-stewart@pm.me>
2026-09-25 11:18:16 -0500

    user/mise: upgrade to 2026.9.13

--- user/mise/template.py
+++ user/mise/template.py
@@ -1,5 +1,5 @@
 pkgname = "mise"
-pkgver = "2026.9.9"
+pkgver = "2026.9.13"
 pkgrel = 0
 build_style = "cargo"
 make_build_args = [
@@ -23,7 +23,7 @@ pkgdesc = "Development environment setup tool"
 license = "MIT"
 url = "https://mise.jdx.dev"
 source = f"https://github.com/jdx/mise/archive/refs/tags/v{pkgver}.tar.gz"
-sha256 = "96b461a471c9f35985611352b9d2c40cd7e66cc52496b1ad9bb64712fec677b2"
+sha256 = "5e3cfaeb4cfaba656fb7532759e8a268da965a76646869ca2215465a4e99aa22"
 # check: takes forever
 options = ["!check"]
 
